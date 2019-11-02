1cca3353a4833d8f92a4c4a3a5c80d4c45ef789f
Shasank <shasank.shah@gmail.com>
2019-11-02 10:10:17 +0530

    list

--- Practise programs/02_11_19/02_11_19.py
+++ Practise programs/02_11_19/02_11_19.py
@@ -19,3 +19,17 @@ print(string[::-1])
 tuple1 = ('a', 'b', 'c')
 tuple2 = (0,) + tuple1
 print(type(tuple2), tuple2)
+
+# To use max and min function in tuple, the elements of it has to be same data type
+tuple3 = ('shell', 'del', 'val')
+print(sorted(tuple3))
+
+tuple4 = tuple3 + ('', 'last')
+print(tuple4)
+
+print(type(tuple2[0]))
+
+list = [1, 2.05, 'stringElement']
+print(id(list), ': ', list)
+list[2]='elementInString'
+print(id(list), ': ', list)
