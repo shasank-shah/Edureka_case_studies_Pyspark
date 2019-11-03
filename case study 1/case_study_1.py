f0fee2e3d1605fd53fa009cf2c9fac861ea2e259
Shasank <shasank.shah@gmail.com>
2019-11-03 16:02:07 +0530

    starting with my exercise

--- case study 1/case_study_1.py
+++ case study 1/case_study_1.py
@@ -1,6 +1,6 @@
 import re
 
-# 1. what is the output of the following code
+# 1. A Robot moves in a Plane starting from the origin point (0,0). The robot can move toward UP, DOWN, LEFT, RIGHT. The trace of Robot movement is as given following:
 nums = set([1, 1, 2, 3, 3, 3, 4, 4])
 print(len(nums))
 # Ans : 4
