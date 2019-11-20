fed80aa8b0b5eae72ff15f24e69db6eb9fb27c12
Amit Banerjee <amit.banerjee@iff.com>
2019-11-20 18:01:25 +0530

    almost completed.

--- case study 3/case_study_3.py
+++ case study 3/case_study_3.py
@@ -2,27 +2,70 @@ import csv
 import re
 from Customer import Customer
 
-_string = 'Mr. Shasank Shah'
-_period = _string.find('.')
-print(type(_period))
-_title = slice(0, 2)
-print(_string[_title])
+class CustomerNotAllowedException(Exception):
+    pass
 
-'''
+def createOrder(_title, _firstName, _lastName, _isBlackListed):
+    try:
+        if _isBlackListed >= 1:
+            raise CustomerNotAllowedException
+        else:
+            print(_title+' '+_firstName+' '+_lastName, "is not black listed")
+    except CustomerNotAllowedException:
+        print(_title + ' ' + _firstName + ' ' + _lastName, "is black listed")
 
-_title = set()
+
 with open('FairDealCustomerData.csv', 'rt') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
+    _lineCount = 0
+    _title = ''
+    _firstName = ''
+    _lastName = ''
     for row in csv_reader:
-        
-        if line_count == 0:            
-            line_count += 1
-        else:
-            unique_jobs.add(row[1])
-            age.append(row[0])
-        
-        xx = row[1]
-        print(xx)
-        r1 = re.findall(r"^\w+", xx)
-        print(r1)
-'''
+        if _lineCount == 0:
+            #print(row[1])
+            _string = row[1]
+            _isBlackListed = int(row[2])
+            #print(_string)
+            if '.' in _string:
+                print(True)
+                _title = (_string[slice(0, _string.find('.') + 1)])
+                _string = _string[_string.find('.') + 1:].lstrip()
+                #print(_title)
+                #print(_string)
+                _whiteSpaceCounter = 0
+                for index in _string:
+                    if (index.isspace()) == True:
+                        #print("here")
+                        _whiteSpaceCounter += 1
+                #print(_whiteSpaceCounter)
+                if _whiteSpaceCounter >= 1:
+                    _firstName = (_string[slice(0, _string.find(' '))])
+                    _string = _string[_string.find(' '):].lstrip()
+                #print(_firstName)
+                #print(_string)
+                if _string != '':
+                    _lastName = _string
+
+                #print("The title is: ", _title, " and the first name is: ", _firstName, " and the last name is: ", _lastName, "asdadad")
+                #print(_title + ' ' + _firstName + ' ' + _lastName)
+                createOrder(_title, _firstName, _lastName, _isBlackListed)
+            else:
+                print(False)
+                _whiteSpaceCounter = 0
+                for index in _string:
+                    if (index.isspace()) == True:
+                        #print("here")
+                        _whiteSpaceCounter += 1
+                #print(_whiteSpaceCounter)
+                if _whiteSpaceCounter >= 1:
+                    _firstName = (_string[slice(0, _string.find(' '))])
+                    _string = _string[_string.find(' '):].lstrip()
+                #print(_firstName)
+                #print(_string)
+                if _string != '':
+                    _lastName = _string
+
+                #print("The title is: ", _title, " and the first name is: ", _firstName, " and the last name is: ", _lastName, "asdadad")
+                #print(_firstName + ' ' + _lastName)
+                createOrder(_title, _firstName, _lastName, _isBlackListed)
