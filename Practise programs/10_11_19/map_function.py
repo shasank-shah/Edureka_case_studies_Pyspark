004f755e5da6a7069a9a38759938e8b05872680d
Amit Banerjee <amit.banerjee@iff.com>
2019-12-23 11:49:25 +0530

    modified code.

--- Practise programs/10_11_19/map_function.py
+++ Practise programs/10_11_19/map_function.py
@@ -4,7 +4,7 @@ def double(x):
     return 2*x
 
 list1 = [1, 2, 3, 4]
-print(list(map(double, list1)))
+print('normal: ', list(map(double, list1)))
 
 # using lambda
-print(list(map(lambda x: x**2, list1)))
+print('lambda: ', list(map(lambda x: x**2, list1)))
